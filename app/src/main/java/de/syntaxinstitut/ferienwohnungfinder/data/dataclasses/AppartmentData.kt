83eb150f1a405e36dc874fd1d9cd466ccad810a5
Kevin Later <laterkevin@icloud.com>
2022-08-16 12:53:40 +0200

    Aufgabe bearbeitet!

--- app/src/main/java/de/syntaxinstitut/ferienwohnungfinder/data/dataclasses/AppartmentData.kt
+++ app/src/main/java/de/syntaxinstitut/ferienwohnungfinder/data/dataclasses/AppartmentData.kt
@@ -1,8 +1,22 @@
 package de.syntaxinstitut.ferienwohnungfinder.data.dataclasses
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 
 // todo: Schreibe hier die Entity rein
+@Entity(tableName = "appartment_table")
 data class AppartmentData(
 //    todo: primary key
+    @PrimaryKey(autoGenerate = true)
+    var title: String,
+    var address: String,
+    var sleepRooms: Int,
+    var size: Int,
+    var persons: Int,
+    var pricePerNight: Int,
+    var image: Int,
+    var petsAllowed: Boolean,
+
     var id: Int = 0
 )
